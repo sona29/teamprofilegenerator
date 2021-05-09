4a2191935cba9225cb8aadbc22108d05faf74095
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-09 22:16:29 +1000

    adding test cases

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -35,6 +35,24 @@ it("Can set name via constructor arguments", () => {
     expect(obj.getName()).toBe(testName);
   });  
 
+  it("Can get id via getId()", () => {
+    const testId = 2;
+    const obj = new Employee("Maja", testId);
+    expect(obj.getId()).toBe(testId);
+  });
+  
+  it("Can get email via getEmail()", () => {
+    const testEmail = "test@test.com";
+    const obj = new Employee("Hannah", 1, testEmail);
+    expect(obj.getEmail()).toBe(testEmail);
+  });
+  
+  it("getRole() should return \"Employee\"", () => {
+    const testValue = "Employee";
+    const obj = new Employee("Henry", 1, "test@test.com");
+    expect(obj.getRole()).toBe(testValue);
+  });
+
 
 });
 
